0b7fe8b7bae1fad19c48d32fcdf0cb9a6c608089
fajar0702 <84577471+fajar0702@users.noreply.github.com>
2021-05-21 21:23:13 +0800

    Create BoXofficex1.js

---
+++ BoXofficex1.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+
+  'https://www.achievablecpmrevenue.com/h5ekpt9rn?key=7c567a589ba175febffee83342e2a22b'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
